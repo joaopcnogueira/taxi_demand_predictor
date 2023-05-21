619e09507d776114132c7728b3a5771b1888ca96
João Paulo Nogueira <joaopcnogueira@gmail.com>
2023-05-21 13:46:28 +0000

    feature pipeline

---
+++ src/config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+from src.paths import PARENT_DIR
+
+# load key-value pairs from .env file located in the parent directory
+load_dotenv(PARENT_DIR / '.env')
+
+HOPSWORKS_PROJECT_NAME = 'taxi_demand_ytjqdg'
+try:
+    HOPSWORKS_API_KEY = os.environ['HOPSWORKS_API_KEY']
+except:
+    raise Exception('Create an .env file on the project root with the HOPSWORKS_API_KEY entry')
+
+
+FEATURE_GROUP_NAME = 'time_series_hourly_feature_group'
+FEATURE_GROUP_VERSION = 1
