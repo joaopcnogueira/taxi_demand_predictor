a2e1e1d5814e1393942c7e1b2a76ce5a718a6a7e
João Paulo Nogueira <joaopcnogueira@gmail.com>
2023-05-23 00:28:57 +0000

    log last event

--- src/paths.py
+++ src/paths.py
@@ -5,6 +5,7 @@ PARENT_DIR = Path(__file__).parent.resolve().parent
 DATA_DIR = PARENT_DIR / 'data'
 RAW_DATA_DIR = PARENT_DIR / 'data' / 'raw'
 TRANSFORMED_DATA_DIR = PARENT_DIR / 'data' / 'transformed'
+LOGS_DIR = PARENT_DIR / 'logs'
 
 MODELS_DIR = PARENT_DIR / 'models'
 
